4deff0fbc571f7b181754ce211afd666846ee90c
Gemechis Chala <gladsonchala@gmail.com>
2023-12-18 03:22:45 +0300

    main.py update

--- api/main.py
+++ api/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from provider.g4frees import G4Frees
+from provider.lexica import LexicaProvider  # Import the LexicaProvider class
 
 app = FastAPI()
 g4frees_instance = G4Frees()
+lexica_instance = LexicaProvider()
 
 @app.post("/api/deebisi/")
 def deebisi_endpoint(user_message: str):
     try:
-        response = g4frees_instance.send_request(user_message)
-        json_response = {"response_code": 200, "message": response}
+        # Use LexicaProvider
+        lexica_response = lexica_instance.send_request(user_message)
+
+        # Use G4Frees provider
+        g4frees_response = g4frees_instance.send_request(user_message)
+
+        # Return a JSON response of both responses
+        json_response = {"response_code": 200, "lexica_message": lexica_response, "g4frees_message": g4frees_response}
         return json_response
     except Exception as e:
         error_message = f"Error processing message: {e}"
